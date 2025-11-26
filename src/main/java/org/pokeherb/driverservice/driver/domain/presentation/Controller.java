2d03b8b5c1248822c8b3071dcd62d3f95caa738c
RCNR <iop04226@gmail.com>
2025-11-26 10:17:28 +0900

    feat: 컨트롤러 작성(#3)

---
+++ src/main/java/org/pokeherb/driverservice/driver/domain/presentation/Controller.java
@@ -0,0 +1,55 @@
+package org.pokeherb.driverservice.driver.domain.presentation;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.pokeherb.driverservice.driver.domain.application.command.DriverCommandService;
+import org.pokeherb.driverservice.driver.domain.application.dto.DriverCreateReqeustDto;
+import org.pokeherb.driverservice.driver.domain.application.dto.DriverUpdateRequestDto;
+import org.pokeherb.driverservice.driver.domain.application.query.DriverQueryService;
+import org.pokeherb.driverservice.driver.domain.entity.DriverType;
+import org.pokeherb.driverservice.driver.domain.entity.dto.DriverDto;
+import org.pokeherb.driverservice.global.infrastructure.CustomResponse;
+import org.pokeherb.driverservice.global.infrastructure.success.GeneralSuccessCode;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequiredArgsConstructor
+@Tag(name = "Driver Controller", description = "Driver 관련 API")
+public class Controller {
+
+    private final DriverQueryService driverQueryService;
+    private final DriverCommandService driverCommandService;
+
+    @GetMapping("{driverId}")
+    public CustomResponse<?> getDriver(@PathVariable("driverId") UUID driverId) {
+        DriverDto driverDto = driverQueryService.getDriver(driverId);
+        return CustomResponse.onSuccess(GeneralSuccessCode.OK, driverDto);
+    }
+
+    @PostMapping("")
+    public CustomResponse<?> createDriver(@RequestBody DriverCreateReqeustDto request) {
+
+        DriverDto driverDto = driverCommandService.createDriver(request);
+        return CustomResponse.onSuccess(GeneralSuccessCode.CREATED, driverDto);
+    }
+
+    @PutMapping("{driverId}")
+    public CustomResponse<?> updateDriverType(
+            @PathVariable("driverId") UUID driverId,
+            @RequestBody DriverUpdateRequestDto requestDto
+    ) {
+        DriverDto driverDto = driverCommandService.updateDriver(driverId, requestDto);
+        return CustomResponse.onSuccess(GeneralSuccessCode.OK, driverDto);
+    }
+
+    @DeleteMapping("{driverId}")
+    public CustomResponse<?> deleteDriver(
+            @RequestHeader("username") String username,
+            @PathVariable("driverId") UUID driverId) {
+
+        driverCommandService.deleteDriver(username, driverId);
+        return CustomResponse.onSuccess(GeneralSuccessCode.OK);
+    }
+}
