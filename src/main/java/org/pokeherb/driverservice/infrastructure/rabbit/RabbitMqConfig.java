1a43ee5403b34e053a4c98691452c5fabcc4257d
RCNR <iop04226@gmail.com>
2025-11-27 19:41:06 +0900

    hotfix: 라우팅 키 수정

--- src/main/java/org/pokeherb/driverservice/infrastructure/rabbit/RabbitMqConfig.java
+++ src/main/java/org/pokeherb/driverservice/infrastructure/rabbit/RabbitMqConfig.java
@@ -10,8 +10,8 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class RabbitMqConfig {
 
-    public static final String DRIVER_ORDER_STATUS_EXCHANGE = "order.dispatch.exchange";
-    public static final String DRIVER_DELIVERY_STATUS_EXCHANGE = "delivery.dispatch.exchange";
+    public static final String DRIVER_ORDER_STATUS_EXCHANGE = "order.status";
+    public static final String DRIVER_DELIVERY_STATUS_EXCHANGE = "delivery.status";
 //    public static final String DRIVER_ASSIGNED_ROUTING_KEY = "order.assigned";
 
     @Bean
